bbfc9d2030596c6ce48789c5f76bd5a6b0a21f7d
fperaltaN <iscfperalta@gmail.com>
2015-10-12 22:16:14 -0500

    Capitulo 3 carpetas.... Unit testing in Django

--- superlits/fuctional_test.py
+++ superlits/fuctional_test.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 import unittest
 
 class NewVisitorTest(unittest.TestCase):#1
-	
+	[...]
 	def setUp(self):#2
 		self.browser = webdriver.Firefox()
 
 	def tearDown(self):#3
 		self.browser.quit()
+	[...]
 
 	def test_can_start_a_list_and_retrieve_it_later(self):#4
 		# Edith has heard about a cool new online to-do app. She goes
