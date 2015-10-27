e1ab2b9d025b7c0c66172fb3db91405d7e316929
Job Daniel Rodriguez Hernandez <REX@linux-ycup.site>
2015-10-26 18:31:50 -0600

    Archios individuales de las pruebas funcionales Capitulo 10

---
+++ superlists/functional_tests/base.py
@@ -0,0 +1,47 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+import sys
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls): 
+        for arg in sys.argv: 
+            if 'liveserver' in arg: 
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return 
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
+
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
